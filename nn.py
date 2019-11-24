364b4f3f59e57c0c7491a74699484179abdd4512
Diego Alcantara <dalcantara7@email.arizona.edu>
2019-11-23 20:11:31 -0700

    got somewhere else

--- nn.py
+++ nn.py
@@ -3,7 +3,8 @@ from nltk.corpus import stopwords
 from keras import models, layers
 import heapq
 # import glove
-
+from keras.preprocessing.text import Tokenizer
+import keras.preprocessing.text as kpt
 import argparse
 import zipfile
 import sklearn.metrics
@@ -13,6 +14,9 @@ import json
 import re
 import emoji
 import string
+import keras
+# import sys
+# np.set_printoptions(threshold=sys.maxsize)
 
 
 emotions = ["anger", "anticipation", "disgust", "fear", "joy", "love",
@@ -28,9 +32,13 @@ def train_and_predict(train_word_data, train_labels,
     # dev_predictions[emotions] = 1
 
     model, kwargs = create_model(vocab_len)
+    # print(len(train_word_data))
+    # print(len(test_word_data))
+    print(train_labels)
+    print(len(test_labels))
 
-    print("train_word[0]", len(train_word_data[0]))
-    print("train_word", len(train_word_data))
+    train_labels = keras.utils.to_categorical(train_labels, 10)
+    test_labels = keras.utils.to_categorical(test_labels, 10)
 
     kwargs.update(x=train_word_data, y=train_labels,
                   epochs=10, validation_data=(test_word_data, test_labels))
@@ -42,94 +50,89 @@ def train_and_predict(train_word_data, train_labels,
 def create_model(vocab_len):
     model = models.Sequential()
     model.add(layers.Embedding(vocab_len, 200, mask_zero=True))
-    model.add(layers.Bidirectional(layers.GRU(64)))
+    model.add(layers.Bidirectional(layers.GRU(64, return_sequences=True)))
     model.add(layers.Dense(50, activation='relu'))
-    model.add(layers.Dense(20, activation ='sigmoid'))
+    model.add(layers.Dense(10, activation ='sigmoid'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     return [model, {}]
 
 def preprocess_data(train, test):
-    # print(train['Tweet'])
-    clean_train_tweets = clean(train['Tweet'])
-    clean_test_tweets = clean(test['Tweet'])
 
-    all_clean_tweets = clean_train_tweets + clean_test_tweets
+    clean_train, clean_test, len_vocab = clean_tweets(train['Tweet'], test['Tweet'])
 
-    wordfreq = {}
-    for sentence in all_clean_tweets:
-        tokens = nltk.word_tokenize(sentence)
-        for token in tokens:
-            if token not in wordfreq.keys():
-                wordfreq[token] = 1
-            else:
-                wordfreq[token] += 1
+    tokenizer = Tokenizer(num_words=len_vocab)
+    tokenizer.fit_on_texts(clean_train)
+    dictionary = tokenizer.word_index
+    with open('graduate-project-dalcantara7/dictionary.json', 'w+') as dictionary_file:
+        json.dump(dictionary, dictionary_file)
 
-    most_freq = heapq.nlargest(sum(wordfreq.values()), wordfreq, key=wordfreq.get)
+    tokenizer.fit_on_texts(clean_test)
+    dictionary = tokenizer.word_index
+    with open('graduate-project-dalcantara7/dictionary.json', 'a') as dictionary_file:
+        json.dump(dictionary, dictionary_file)
+    
+    train_tweet_indices = []
+    for tweet in clean_train:
+        word_indices = convert_tweet_to_index_array(dictionary, tweet)
+        train_tweet_indices.append(word_indices)
 
-    train_sentence_vectors = []
-    for sentence in clean_train_tweets:
-        sentence_tokens = nltk.word_tokenize(sentence)
-        sent_vec = []
-        for token in most_freq:
-            if token in sentence_tokens:
-                sent_vec.append(1)
-            else:
-                sent_vec.append(0)
-        train_sentence_vectors.append(sent_vec)
+    train_tweet_indices = np.asarray(train_tweet_indices)
 
-    test_sentence_vectors = []
-    for sentence in clean_test_tweets:
-        sentence_tokens = nltk.word_tokenize(sentence)
-        sent_vec = []
-        for token in most_freq:
-            if token in sentence_tokens:
-                sent_vec.append(1)
-            else:
-                sent_vec.append(0)
-        test_sentence_vectors.append(sent_vec)
+    test_tweet_indices = []
+    for tweet in clean_test:
+        word_indices = convert_tweet_to_index_array(dictionary, tweet)
+        test_tweet_indices.append(word_indices)
 
-    train_sentence_vectors = np.asarray(train_sentence_vectors)
-    test_sentence_vectors = np.asarray(test_sentence_vectors)
+    test_tweet_indices = np.asarray(test_tweet_indices)
 
-    len_vocab = len(wordfreq.keys())
-    print(len_vocab)
+    train_tweet_matrix = tokenizer.sequences_to_matrix(train_tweet_indices, mode='binary')
+    test_tweet_matrix = tokenizer.sequences_to_matrix(test_tweet_indices, mode='binary')
 
-    train_mat = np.matrix(train_sentence_vectors)
-    with open('graduate-project-dalcantara7/train_matrix.txt', 'w+') as f:
-        for line in train_mat:
-            np.savetxt(f, line, fmt='%1.0f')
+    return train_tweet_matrix, test_tweet_matrix, len_vocab
 
-    test_mat = np.matrix(test_sentence_vectors)
-    with open('graduate-project-dalcantara7/test_matrix.txt', 'w+') as f:
-        for line in test_mat:
-            np.savetxt(f, line, fmt='%1.0f')
+def convert_tweet_to_index_array(dictionary, tweet):
+    return [dictionary[word] for word in kpt.text_to_word_sequence(tweet)]
 
-    return train_sentence_vectors, test_sentence_vectors, len_vocab
+def clean_tweets(train_set, test_set):
+    clean_train = []
+    clean_test = []
 
-    #use bidirectional RNN for model
-
-def clean(tweet_set):
-    to_return = []
-
-    for tweet in tweet_set:
+    for tweet in train_set:
         demojized = emoji.demojize(tweet)
         lowered = demojized.lower()
         sans_special_chars = re.sub(r'\W',' ', lowered)
         sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
-        to_return.append(sans_extra_spaces)
-        # array_words = sans_extra_spaces.split()
-        # to_return += [word for word in array_words if word not in set(stopwords.words('english'))]
-        # print(to_return)
-    
-    return to_return
+        split = sans_extra_spaces.split()
+        without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
+        # vocabulary += [word for word in clean_train]
+        clean_train.append(without_stop_words)
+
+    for tweet in test_set:
+        demojized = emoji.demojize(tweet)
+        lowered = demojized.lower()
+        sans_special_chars = re.sub(r'\W',' ', lowered)
+        sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
+        split = sans_extra_spaces.split()
+        without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
+        # vocabulary += [word for word in clean_train]
+        clean_test.append(without_stop_words)
+
+    len_vocab = 0
+    for tweet in clean_train:
+        len_vocab += len(tweet)
+
+    for tweet in clean_test:
+        len_vocab += len(tweet)
+
+    return clean_train, clean_test, len_vocab
 
 if __name__ == "__main__":
     # gets the training and test file names from the command line
     parser = argparse.ArgumentParser()
     parser.add_argument("train", nargs='?', default="graduate-project-dalcantara7/2018-E-c-En-train.txt")
-    parser.add_argument("test", nargs='?', default="graduate-project-dalcantara7/2018-E-c-En-train.txt")
+    parser.add_argument("test", nargs='?', default="graduate-project-dalcantara7/2018-E-c-En-dev.txt")
     args = parser.parse_args()
 
     # reads train and dev data into Pandas data frames
@@ -137,14 +140,15 @@ if __name__ == "__main__":
                            converters={e: emotion_to_int.get for e in emotions})
     train_data = pd.read_csv(args.train, **read_csv_kwargs)
     test_data = pd.read_csv(args.test, **read_csv_kwargs)
+    # print(train_data.iloc[:,2:11][0])
 
-    train_word_matrix, test_word_matrix, vocab_len = preprocess_data(train_data, test_data)
+    train_word_indices, test_word_indices, vocab_len = preprocess_data(train_data, test_data)
+
+    train_labels = train_data.iloc[:,2:11].values
+    test_labels = test_data.iloc[:,2:11].values
 
     # makes predictions on the dev set
-    train_matrix = np.loadtxt("graduate-project-dalcantara7/train_matrix.txt", dtype='i', delimiter=' ')
-    test_matrix = np.loadtxt("graduate-project-dalcantara7/test_matrix.txt", dtype='i', delimiter=' ')
-    vocab_len = 16052
-    test_predictions = train_and_predict(train_matrix, test_matrix, np.asarray(train_data.iloc[:,2:11]), np.asarray(test_data.iloc[:,2:11]), vocab_len)
+    test_predictions = train_and_predict(train_word_indices, test_word_indices, train_labels, test_labels, vocab_len)
 
     # saves predictions and creates submission zip file
     test_predictions.to_csv("E-C_en_pred.txt", sep="\t", index=False)
