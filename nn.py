8c2665f0a4b5b661ce63bdd51d68964bc88657ed
Diego Alcantara <dalcantara7@email.arizona.edu>
2019-11-25 17:59:10 -0700

    about 44% accuracy

--- nn.py
+++ nn.py
@@ -19,7 +19,7 @@ emotion_to_int = {"0": 0, "1": 1, "NONE": -1}
 
 
 def train_and_predict(train_word_data, train_labels,
-                      test_word_data, test_labels, vocab_len):
+                      test_word_data, test_labels, vocab_len, full_test_data):
 
     # doesn't train anything; just predicts 1 for all of dev set
     # dev_predictions = dev_data.copy()
@@ -28,11 +28,28 @@ def train_and_predict(train_word_data, train_labels,
     model, kwargs = create_model(vocab_len)
 
     kwargs.update(x=train_word_data, y=train_labels,
-                  epochs=10, validation_data=(test_word_data, test_labels))
+                  epochs=13, validation_data=(test_word_data, test_labels))
 
     model.fit(**kwargs)
 
-    return model.predict(test_word_data, test_labels)
+    dev_predictions = full_test_data.copy()
+
+    predictions = model.predict(test_word_data)
+    # mat = np.matrix(predictions)
+    # with open('graduate-project-dalcantara7/predictions_continuous.txt', 'w+') as f:
+    #     for line in mat:
+    #         np.savetxt(f, line, fmt='%.2f')
+
+    predictions = np.round(predictions)
+    # mat = np.matrix(predictions)
+    # with open('graduate-project-dalcantara7/predictions.txt', 'w+') as f:
+    #     for line in mat:
+    #         np.savetxt(f, line, fmt='%1.0f')
+
+    dev_predictions[emotions] = predictions
+    # print(dev_predictions)
+
+    return dev_predictions
 
 def create_model(vocab_len):
     model = models.Sequential()
@@ -46,10 +63,18 @@ def create_model(vocab_len):
     return [model, {}]
 
 def preprocess_data(train, test):
+    clean_train = []
+    clean_test = []
 
-    clean_train, clean_test, len_vocab = clean_tweets(train['Tweet'], test['Tweet'])
+    with open('graduate-project-dalcantara7/clean_train_tweets.txt', 'r') as f:
+        for tweet in f:
+            clean_train.append(tweet.rstrip())
 
-    tokenizer = Tokenizer(num_words=len_vocab)
+    with open('graduate-project-dalcantara7/clean_test_tweets.txt', 'r') as f:
+        for tweet in f:
+            clean_test.append(tweet.rstrip())
+
+    tokenizer = Tokenizer(num_words=120000)
     tokenizer.fit_on_texts(clean_train)
     
     train_tweet_indices = tokenizer.texts_to_sequences(clean_train)
@@ -58,7 +83,7 @@ def preprocess_data(train, test):
     test_tweet_indices = tokenizer.texts_to_sequences(clean_test)
     test_tweet_indices = kps.pad_sequences(test_tweet_indices, padding='post')
 
-    return train_tweet_indices, test_tweet_indices, len_vocab
+    return train_tweet_indices, test_tweet_indices
 
 def clean_tweets(train_set, test_set):
     clean_train = []
@@ -69,8 +94,8 @@ def clean_tweets(train_set, test_set):
         lowered = demojized.lower()
         sans_special_chars = re.sub(r'\W',' ', lowered)
         sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
-        #remove underscores string.replace('_', ' ')
-        split = sans_extra_spaces.split()
+        sans_underscore = sans_extra_spaces.replace('_', ' ')
+        split = sans_underscore.split()
         without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
         clean_train.append(without_stop_words)
 
@@ -79,7 +104,8 @@ def clean_tweets(train_set, test_set):
         lowered = demojized.lower()
         sans_special_chars = re.sub(r'\W',' ', lowered)
         sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
-        split = sans_extra_spaces.split()
+        sans_underscore = sans_extra_spaces.replace('_', ' ')
+        split = sans_underscore.split()
         without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
         clean_test.append(without_stop_words)
 
@@ -87,7 +113,15 @@ def clean_tweets(train_set, test_set):
     for tweet in clean_train:
         len_vocab += len(tweet)
 
-    return clean_train, clean_test, len_vocab
+    with open('graduate-project-dalcantara7/clean_train_tweets.txt', 'w+') as f:
+        for tweet in clean_train:
+            f.write("%s\n" % tweet)
+
+    with open('graduate-project-dalcantara7/clean_test_tweets.txt', 'w+') as f:
+        for tweet in clean_test:
+            f.write("%s\n" % tweet)
+
+    # return clean_train, clean_test, len_vocab
 
 if __name__ == "__main__":
     # gets the training and test file names from the command line
@@ -103,19 +137,21 @@ if __name__ == "__main__":
     test_data = pd.read_csv(args.test, **read_csv_kwargs)
     # print(train_data.iloc[:,2:11][0])
 
-    train_word_indices, test_word_indices, vocab_len = preprocess_data(train_data, test_data)
+    # clean_tweets(train_data['Tweet'], test_data['Tweet'])
 
-    train_labels = train_data.iloc[:,2:13]
-    test_labels = test_data.iloc[:,2:13]
+    train_word_indices, test_word_indices = preprocess_data(train_data, test_data)
+
+    train_labels = np.asarray(train_data.iloc[:,2:13])
+    test_labels = np.asarray(test_data.iloc[:,2:13])
 
     # makes predictions on the dev set
-    test_predictions = train_and_predict(train_word_indices, train_labels, test_word_indices, test_labels, vocab_len)
-
-    # saves predictions and creates submission zip file
-    test_predictions.to_csv("E-C_en_pred.txt", sep="\t", index=False)
-    with zipfile.ZipFile('submission.zip', mode='w') as submission_zip:
-        submission_zip.write("E-C_en_pred.txt")
+    test_predictions = train_and_predict(train_word_indices, train_labels, test_word_indices, test_labels, 120000, test_data)
 
     # prints out multi-label accuracy
     print("accuracy: {:.3f}".format(sklearn.metrics.jaccard_similarity_score(
         test_data[emotions], test_predictions[emotions])))
+
+    # saves predictions and creates submission zip file
+    test_predictions.to_csv("graduate-project-dalcantara7/E-C_en_pred.txt", sep="\t", index=False)
+    with zipfile.ZipFile('graduate-project-dalcantara7/submission.zip', mode='w') as submission_zip:
+        submission_zip.write("graduate-project-dalcantara7/E-C_en_pred.txt")
