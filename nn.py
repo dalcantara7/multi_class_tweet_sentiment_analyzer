75e04fcff58bee326f831b4fd916f16abfef3392
Diego Alcantara <dalcantara7@email.arizona.edu>
2019-11-27 15:27:55 -0700

    0.49 jaccard score

--- nn.py
+++ nn.py
@@ -12,6 +12,8 @@ import re
 import emoji
 import keras
 from keras import backend as K
+from keras import callbacks
+from nltk.stem.snowball import SnowballStemmer
 
 emotions = ["anger", "anticipation", "disgust", "fear", "joy", "love",
             "optimism", "pessimism", "sadness", "surprise", "trust"]
@@ -24,7 +26,7 @@ def train_and_predict(train_word_data, train_labels,
     model, kwargs = create_model(vocab_len)
 
     kwargs.update(x=train_word_data, y=train_labels,
-                  epochs=3, validation_data=(test_word_data, test_labels))
+                  epochs=4, validation_data=(test_word_data, test_labels))
 
     model.fit(**kwargs)
 
@@ -40,21 +42,16 @@ def train_and_predict(train_word_data, train_labels,
 
 def create_model(vocab_len):
     model = models.Sequential()
-    model.add(layers.Embedding(vocab_len, 300, mask_zero=True))
-    model.add(layers.Bidirectional(layers.GRU(128)))
+    model.add(layers.Embedding(vocab_len, 100, mask_zero=True))
+    model.add(layers.Bidirectional(layers.GRU(250)))
+    # model.add(layers.Bidirectional(layers.LSTM(50)))
     model.add(layers.Dense(100, activation='relu'))
     model.add(layers.Dense(11, activation ='sigmoid'))
 
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    return [model, {}]
-
-def jss(y_true, y_pred):
-    smooth=100
-    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
-    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
-    jac = (intersection + smooth) / (sum_ - intersection + smooth)
-    return (1 - jac) * smooth
+    return [model, {'batch_size' : 64}]
+    # return [model, {}]
 
 def preprocess_data(train, test):
     clean_train = []
@@ -68,7 +65,7 @@ def preprocess_data(train, test):
         for tweet in f:
             clean_test.append(tweet.rstrip())
 
-    tokenizer = Tokenizer(num_words=120000)
+    tokenizer = Tokenizer(num_words=90000)
     tokenizer.fit_on_texts(clean_train)
     
     train_tweet_indices = tokenizer.texts_to_sequences(clean_train)
@@ -82,6 +79,7 @@ def preprocess_data(train, test):
 def clean_tweets(train_set, test_set):
     clean_train = []
     clean_test = []
+    sno = SnowballStemmer('english')
 
     for tweet in train_set:
         demojized = emoji.demojize(tweet)
@@ -90,8 +88,8 @@ def clean_tweets(train_set, test_set):
         sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
         sans_underscore = sans_extra_spaces.replace('_', ' ')
         split = sans_underscore.split()
-        without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
-        clean_train.append(without_stop_words)
+        stemmed = ' '.join([sno.stem(word) for word in split])
+        clean_train.append(stemmed)
 
     for tweet in test_set:
         demojized = emoji.demojize(tweet)
@@ -100,8 +98,8 @@ def clean_tweets(train_set, test_set):
         sans_extra_spaces = re.sub(r'\s+',' ', sans_special_chars)
         sans_underscore = sans_extra_spaces.replace('_', ' ')
         split = sans_underscore.split()
-        without_stop_words = ' '.join([word for word in split if word not in set(stopwords.words('english'))])
-        clean_test.append(without_stop_words)
+        stemmed = ' '.join([sno.stem(word) for word in split])
+        clean_test.append(stemmed)
 
     len_vocab = 0
     for tweet in clean_train:
@@ -114,8 +112,6 @@ def clean_tweets(train_set, test_set):
     with open('graduate-project-dalcantara7/clean_test_tweets.txt', 'w+') as f:
         for tweet in clean_test:
             f.write("%s\n" % tweet)
-
-    # return clean_train, clean_test, len_vocab
 
 if __name__ == "__main__":
     # gets the training and test file names from the command line
@@ -138,7 +134,7 @@ if __name__ == "__main__":
     test_labels = np.asarray(test_data.iloc[:,2:13])
 
     # makes predictions on the dev set
-    test_predictions = train_and_predict(train_word_indices, train_labels, test_word_indices, test_labels, 120000, test_data)
+    test_predictions = train_and_predict(train_word_indices, train_labels, test_word_indices, test_labels, 90000, test_data)
 
     # prints out multi-label accuracy
     print("accuracy: {:.3f}".format(sklearn.metrics.jaccard_similarity_score(
